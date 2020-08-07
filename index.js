ea0a2b1ea342d52749ebb7252385013c569db4ad
gokulcodes <gokul282@outlook.com>
2020-08-07 17:21:01 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const express = require('express')
+const ejs = require('ejs')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const {v4: uuidV4} = require('uuid')
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    console.log('called')
-    res.redirect(`/${uuidV4}`)
+    res.redirect(`${uuidV4()}`)
 })
 app.get('/:room', (req, res) => {
-    console.log(req.params.room)
     res.render('room', {roomID: req.params.room})
 })
-// io.on('connection', (socket) => {
-//     console.log('user-connected')
-// })
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomID, userId) => {
+    socket.join(roomID)
+    socket.to(roomID).broadcast.emit('user-connected', userId)
+    socket.on('disconnect', () => {
+      socket.to(roomID).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
 
 server.listen(3000, () => {
     console.log('connected to 3000')
--- public/socket.js
+++ public/socket.js
@@ -1,1 +1,57 @@
-const socket = io('http://localhost:3000')
+const socket = io('/')
+const videoGrid = document.querySelector('.videoGrid')
+const mypeer = new Peer(undefined, {
+    host: '/',
+    port: 3001
+})
+const peers = []
+mypeer.on('open', id => {
+    socket.emit('join-room', roomID, id)
+})
+
+socket.on('user-connected', (userId) => {
+    console.log("user-connected"+ userId)
+})
+
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true
+}).then(stream => {
+    createVideoSource(stream, myVideo)
+    mypeer.on('call', (call) => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', (userVideoStream) => {
+            createVideoSource(userVideoStream, video)
+        })
+    })
+    socket.on('user-connected', userId => {
+        connectTonewUser(stream, userId)
+    })
+    socket.on('user-disconnected', userId => {
+        if(peers[userId]) peers[userId].close()
+    })
+})
+
+function createVideoSource(stream, video){
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
+
+function connectTonewUser(stream, userId){
+    const call = mypeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        createVideoSource(userVideoStream, video)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+    peers[userId] = call
+}
